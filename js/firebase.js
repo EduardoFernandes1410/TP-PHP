d70a20e8e97c552a87d5e9954536a554e797c35e
Eduardo Fernandes <eduardo.militao1410@gmail.com>
2017-07-04 13:22:25 -0300

    Reestruturacao + Login

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+// Inicializa Firebase
+var config = {
+	apiKey: "AIzaSyAmx6eClyN7rYC00XTEuuym16Qe1KaC5P0",
+    authDomain: "teachme-562d8.firebaseapp.com",
+    databaseURL: "https://teachme-562d8.firebaseio.com",
+    projectId: "teachme-562d8",
+    storageBucket: "teachme-562d8.appspot.com",
+    messagingSenderId: "632001666902"
+};
+firebase.initializeApp(config);

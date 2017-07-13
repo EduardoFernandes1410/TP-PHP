5d7ff3c6656a33ff2e94b1ac562599a96d80f066
Lucas <lucascoltec2016@gmail.com>
2017-07-13 11:18:19 -0300

    Seção VIRAR SENSEI Completa!
    Só falta tirar os "style" 's

--- js/home.js
+++ js/home.js
@@ -7,7 +7,13 @@ $(document).ready(function(){
 
 	$('.modal').modal();
 
+	// Comandos do JQuery Mask Plugin: http://igorescobar.github.io/jQuery-Mask-Plugin/
+
 	$('.cpf').mask('000.000.000-00', {
 		reverse: false
 	});
+
+	$('.phone_with_ddd').mask('+00 (00) 00000-0000');
+
+	$('.cep').mask('00000-000');
 });

d3c3ecfb62da51677011008e1aa51bd034b98bc9
Lucas Macedo Bruzzi <lucascoltec2016@gmail.com>
2017-08-01 23:29:36 -0300

    Merge manual do requisito R1 - Virar Sensei!

--- js/home.js
+++ js/home.js
@@ -6,4 +6,37 @@ $(document).ready(function(){
 	});
 
 	$('.modal').modal();
+
+	// Comandos do JQuery Mask Plugin: http://igorescobar.github.io/jQuery-Mask-Plugin/
+
+	$('.cpf').mask('000.000.000-00', {
+		reverse: false
+	});
+
+	$('.phone_with_ddd').mask('+00 (00) 00000-0000');
 });
+
+var enviaFormularioSensei = function(){
+	var cadastroUsuario = {
+		Cpf: document.getElementById('cpf'),
+		Fone: document.getElementById('contato'),
+		Cidade: document.getElementById('cidade'),
+		Rua: document.getElementById('rua'),
+		Numero: document.getElementById('numero'),
+		Complemento: document.getElementById('complemento')
+	};
+
+	cadastroUsuario = JSON.stringify(cadastroUsuario);
+
+	$.ajax({
+		data: 'cadastroUsuario=' + cadastroUsuario,
+		url: "../backend/user/virasensei.php",
+		method: 'POST',
+		success: function(){
+			console.log("Cadastro enviado para o Developer Team!");
+		},
+		error: function(){
+			console.log("Formulário inválido!");
+		}
+	});
+}
